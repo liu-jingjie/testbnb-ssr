550facb498b88e1c4997447513718801e2199c90
RyuuKeiKetu <1793842@gmail.com>
2024-04-20 00:10:17 +0900

    实现了vue-router的例子。

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { useRouter } from 'vue-router'
+const router = useRouter();
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <button @click="() => router.push({path:'/home'})">首页</button>
+  <button @click="() => router.push({path:'/mine'})">个人中心</button>
+
+  <router-link to="/home">首页</router-link>
+  <router-link to="/mine">个人中心</router-link>
+
+
+  <br>
+  <router-view />
 </template>
 
 <style scoped>
