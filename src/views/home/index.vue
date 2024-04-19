8914a9c6b81f3783abf841117aa1a388df3c67ba
RyuuKeiKetu <1793842@gmail.com>
2024-04-20 01:14:41 +0900

    element-plus的导入

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import { useRouter, useRoute } from 'vue-router'
+import { getCurrentInstance, h } from 'vue'
 const router = useRouter();
 const route = useRoute();
 
 console.log(route.params)
+const {proxy}:any = getCurrentInstance();
+proxy.$message({
+    message: h('p', { style: 'line-height: 1; font-size: 14px' }, [
+      h('span', null, 'Message can be '),
+      h('i', { style: 'color: teal' }, 'VNode'),
+    ]),
+  })
 </script>
 
 <template>
   首页
   <button @click="() => router.push({path:'/mine', query:{id: 1}})">跳转到个人中心</button>
+  <el-button type="primary" plain>Primary</el-button>
 </template>
 
 <style scoped>
